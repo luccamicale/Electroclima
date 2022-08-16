16ed145b0a66b847e26f9aab2c076ffd255a6f47
luccamicale <luccamicale2015@gmail.com>
2022-08-16 12:56:07 -0300

    Tienda Onlina

---
+++ slider.js/slider1.js
@@ -0,0 +1,80 @@
+let password= "";
+while(password!=="1234"){
+    password=prompt("Introduzca Contrasena");
+}
+
+function abrir1(){
+    document.getElementById("vent1").style.display="block";
+};
+function cerrar1(){
+    document.getElementById("vent1").style.display="none";
+}
+function abrir2(){
+    document.getElementById("vent2").style.display="block"
+};
+function cerrar2(){
+    document.getElementById("vent2").style.display="none"
+};
+
+function abrir3(){
+    document.getElementById("vent3").style.display="block"
+};
+function cerrar3(){
+    document.getElementById("vent3").style.display="none"
+};
+function abrir4(){
+    document.getElementById("vent4").style.display="block";
+};
+function cerrar4(){
+    document.getElementById("vent4").style.display="none";
+}
+function abrir5(){
+    document.getElementById("vent5").style.display="block"
+};
+function cerrar5(){
+    document.getElementById("vent5").style.display="none"
+};
+
+function abrir6(){
+    document.getElementById("vent6").style.display="block"
+};
+function cerrar6(){
+    document.getElementById("vent6").style.display="none"
+};
+function abrir7(){
+    document.getElementById("vent7").style.display="block"
+};
+function cerrar7(){
+    document.getElementById("vent7").style.display="none"
+};
+function abrir8(){
+    document.getElementById("vent8").style.display="block";
+};
+function cerrar8(){
+    document.getElementById("vent8").style.display="none";
+}
+function abrir9(){
+    document.getElementById("vent9").style.display="block"
+};
+function cerrar9(){
+    document.getElementById("vent9").style.display="none"
+};
+function abrir10(){
+    document.getElementById("vent10").style.display="block"
+};
+function cerrar10(){
+    document.getElementById("vent10").style.display="none"
+};
+function abrir11(){
+    document.getElementById("vent11").style.display="block";
+};
+function cerrar11(){
+    document.getElementById("vent11").style.display="none";
+}
+function abrir12(){
+    document.getElementById("vent12").style.display="block"
+};
+function cerrar12(){
+    document.getElementById("vent12").style.display="none"
+};
+
